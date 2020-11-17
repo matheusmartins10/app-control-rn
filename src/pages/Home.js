1fb9a102fbaba151b692f4cec453beb1ae789e55
Matheus Martins <mmartins.oliveira@outlook.com>
2020-11-16 23:03:16 -0300

    new files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+
+
 import styled from 'styled-components/native' 
 import { useNavigation } from '@react-navigation/native'
 
@@ -11,11 +13,26 @@ export default function Home() {
         navigation.navigate('Order')
     }
 
+    function navigationToDelivery(){
+        navigation.navigate('Delivery')
+    }
+
+    function navigationToEquipment(){
+       navigation.navigate('Equipment')
+    }
+
     return (
         <Container>
+            <Logo source={{uri: 'http://169.57.149.88/casanew/wp-content/uploads/2020/03/CBO_LATINCBO@2x.png'}}  />
             <Title> Olá, seja bem-vindo!</Title>
             <NextOrder>
                <SubTitle onPress={() => navigationToOrder()} > Pedido de equipamento </SubTitle>
+            </NextOrder>
+            <NextOrder>
+               <SubTitle onPress={() => navigationToDelivery()} > Entrega de equipamento </SubTitle>
+            </NextOrder>
+            <NextOrder>
+               <SubTitle onPress={() => navigationToEquipment()} > Equipamento com defeito </SubTitle>
             </NextOrder>
         </Container>
     )
@@ -25,17 +42,33 @@ const Container = styled.View`
     flex: 1;
     align-items: center;
     justify-content: flex-start;
-    margin-top: 100px;
+    margin-top: 160px;
 `
+
+const Logo = styled.Image`
+   width: 150px;
+   height: 60px;
+`
+
 const Title = styled.Text`
-   font-size: 32px;
+   font-size: 28px;
    font-weight: bold;
-   font-family: sans-serif;
+   margin: 60px;
 `
 
 const SubTitle = styled.Text`
    font-size: 18;
+   color: #fff;
+   margin: 10px auto;
 `
 
-const NextOrder = styled.TouchableOpacity``
+const NextOrder = styled.TouchableOpacity`
+   flex-direction: row;
+   justify-content: space-between;
+   align-items: center;
+   background: red;
+   width: 300px;
+   margin: 10px;
+   border-radius: 10px;
+`
 
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ const AppStack = createStackNavigator()
 
 import Home from './pages/Home'
 import Order from './pages/Order'
+import Delivery from './pages/Delivery'
+import Equipment from './pages/Equipment'
 
 export default function Routes(){
     return (
@@ -13,6 +15,8 @@ export default function Routes(){
             <AppStack.Navigator screenOptions={{headerShown: false}}>
                <AppStack.Screen name="Home" component={Home} />
                <AppStack.Screen name="Order" component={Order}  />
+               <AppStack.Screen name="Delivery" component={Delivery} />
+               <AppStack.Screen name="Equipment" component={Equipment} />
             </AppStack.Navigator>
         </NavigationContainer>
     )
